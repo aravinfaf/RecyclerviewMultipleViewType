e2093f4618ada413ce1286f84ec6e25fa2554144
aravind <aravindhraj283@gmail.com>
2022-03-23 11:02:11 +0530

    Databinding & Hilt implementation

--- app/src/main/java/com/aravind/recyclerviewviewtype/view/activity/PlatformActivity.kt
+++ app/src/main/java/com/aravind/recyclerviewviewtype/view/activity/PlatformActivity.kt
@@ -1,42 +1,41 @@
 package com.aravind.recyclerviewviewtype.view.activity
 
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.aravind.recyclerviewviewtype.R
+import com.aravind.recyclerviewviewtype.databinding.ActivityMainBinding
 import com.aravind.recyclerviewviewtype.model.Android
 import com.aravind.recyclerviewviewtype.model.IOS
 import com.aravind.recyclerviewviewtype.model.Platform
 import com.aravind.recyclerviewviewtype.model.Windows
 import com.aravind.recyclerviewviewtype.view.adapter.PlatformAdapter
+import com.aravind.recyclerviewviewtype.viewmodel.PlatformViewmodel
+import dagger.hilt.android.AndroidEntryPoint
+import java.lang.Exception
 
+@AndroidEntryPoint
 class PlatformActivity : AppCompatActivity() {
 
-    var list : ArrayList<Platform> = arrayListOf()
-    private lateinit var recyclerview : RecyclerView
-    lateinit var adapter : PlatformAdapter
+    lateinit var binding : ActivityMainBinding
+    private  val viewModel : PlatformViewmodel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
-        recyclerview = findViewById(R.id.recyclerview)
-
-        recyclerview.apply {
+        binding.recyclerview.apply {
             layoutManager = LinearLayoutManager(applicationContext)
         }
-
-        list.add(Platform(1, Android("Noughat","7.0", R.drawable.whatsapp)))
-        list.add(Platform(1, Android("Oreo","8.0", R.drawable.facebook)))
-        list.add(Platform(1, Android("Noughat","7.0", R.drawable.twitter)))
-
-        list.add(Platform(2, IOS("IOS","12.0", R.drawable.instagram)))
-        list.add(Platform(2, IOS("IOS","13.0", R.drawable.facebook)))
-
-        list.add(Platform(3, Windows("Windows","10.0")))
-        list.add(Platform(3, Windows("Windows","7.0")))
-
-        recyclerview.adapter = PlatformAdapter(list)
+        viewModel.getPlatform()
+        viewModel.listValue.observeForever{
+            binding.recyclerview.adapter = PlatformAdapter(it)
+        }
     }
 }
--- app/src/main/java/com/aravind/recyclerviewviewtype/view/adapter/PlatformAdapter.kt
+++ app/src/main/java/com/aravind/recyclerviewviewtype/view/adapter/PlatformAdapter.kt
@@ -1,12 +1,11 @@
 package com.aravind.recyclerviewviewtype.view.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.aravind.recyclerviewviewtype.R
+import com.aravind.recyclerviewviewtype.databinding.ItemAndroidBinding
+import com.aravind.recyclerviewviewtype.databinding.ItemIosBinding
+import com.aravind.recyclerviewviewtype.databinding.ItemWindowsBinding
 import com.aravind.recyclerviewviewtype.model.Android
 import com.aravind.recyclerviewviewtype.model.IOS
 import com.aravind.recyclerviewviewtype.model.Platform
@@ -17,69 +16,49 @@ class PlatformAdapter(private val list : List<Platform>) : RecyclerView.Adapter<
     companion object {
         const val VIEW_TYPE_ONE = 1
         const val VIEW_TYPE_TWO = 2
-        const val VIEW_TYPE_THREE = 3
     }
 
-    inner class AndroidViewHolder(private val view : View) : RecyclerView.ViewHolder(view){
-        private val image : ImageView = view.findViewById(R.id.androidImage)
-        private val title : TextView = view.findViewById(R.id.androidTitle)
-        private val version : TextView = view.findViewById(R.id.androidVersion)
+    inner class AndroidViewHolder(private val binding : ItemAndroidBinding) : RecyclerView.ViewHolder(binding.root){
 
         fun setAndroidData(androidPlatform : Android){
-            image.setImageResource(androidPlatform.image)
-            title.text = androidPlatform.os
-            version.text = androidPlatform.version
+            binding.androidImage.setImageResource(androidPlatform.image)
+            binding.androidTitle.text = androidPlatform.os
+            binding.androidVersion.text = androidPlatform.version
         }
     }
 
-    inner class IOSViewHolder(val view : View) : RecyclerView.ViewHolder(view){
-        private val image : ImageView = view.findViewById(R.id.iosImage)
-        private val title : TextView = view.findViewById(R.id.iosTitle)
-        private val version : TextView = view.findViewById(R.id.iosVersion)
+    inner class IOSViewHolder(private val binding : ItemIosBinding) : RecyclerView.ViewHolder(binding.root){
 
         fun setIOSData(iosPlatform : IOS){
-            image.setImageResource(iosPlatform.image)
-            title.text = iosPlatform.os
-            version.text = iosPlatform.version
+            binding.iosImage.setImageResource(iosPlatform.image)
+            binding.iosTitle.text = iosPlatform.os
+            binding.iosVersion.text = iosPlatform.version
         }
     }
-    inner class WindowsViewHolder(val view : View) : RecyclerView.ViewHolder(view){
-        private val title : TextView = view.findViewById(R.id.windowsTitle)
-        private val version : TextView = view.findViewById(R.id.windowsVersion)
-
-        fun setWindowsData(windowsPlatform : Windows){
-            title.text = windowsPlatform.os
-            version.text = windowsPlatform.version
+    inner class WindowsViewHolder(private val binding: ItemWindowsBinding) : RecyclerView.ViewHolder(binding.root){
+         fun setWindowsData(windowsPlatform : Windows){
+             binding.windowsTitle.text = windowsPlatform.os
+             binding.windowsVersion.text = windowsPlatform.version
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-
         return when(viewType){
-            VIEW_TYPE_ONE ->  AndroidViewHolder(
-                LayoutInflater.from(parent.context).inflate(R.layout.item_android,parent,false)
-            )
-            VIEW_TYPE_TWO ->  IOSViewHolder(
-                LayoutInflater.from(parent.context).inflate(R.layout.item_ios,parent,false)
-            )
-            else ->  WindowsViewHolder(
-                LayoutInflater.from(parent.context).inflate(R.layout.item_windows,parent,false)
-            )
+            VIEW_TYPE_ONE ->
+                AndroidViewHolder(ItemAndroidBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+            VIEW_TYPE_TWO ->
+                IOSViewHolder(ItemIosBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+            else ->  WindowsViewHolder(ItemWindowsBinding.inflate(LayoutInflater.from(parent.context),parent,false))
         }
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        if (list[position].type == VIEW_TYPE_ONE){
-                val androidData = list[position].obj as Android
-            (holder as AndroidViewHolder).setAndroidData(androidData)
-        }else if (list[position].type == VIEW_TYPE_TWO){
-            val iosData : IOS = list[position].obj as IOS
-            (holder as IOSViewHolder).setIOSData(iosData)
+
+        when(list[position].type){
+            VIEW_TYPE_ONE -> (holder as AndroidViewHolder).setAndroidData(list[position].obj as Android)
+            VIEW_TYPE_TWO -> (holder as IOSViewHolder).setIOSData(list[position].obj as IOS)
+            else -> (holder as WindowsViewHolder).setWindowsData(list[position].obj as Windows)
         }
-//        else{
-//            val windowsData : Windows = list[position].obj as Windows
-//            (holder as WindowsViewHolder).setWindowsData(windowsData)
-//        }
     }
 
     override fun getItemCount(): Int = list.size
